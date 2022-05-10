82822119f58f5fe9d585f391c3cd09e5eee62fb2
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-10 15:06:23 -0300

    create welcome view

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import "./assets/styles/global.css"
 import Footer from './components/Footer';
+import Welcome from './components/views/Welcome';
 
 const App = () => {
   return (
     <>
       <NavBar/>
+      <Welcome/>
       <Footer/>
     </>
   );
