d66dec183e016ed8dfd52f7475afa159a464d454
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-10 14:59:09 -0300

    create footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar';
 import "./assets/styles/global.css"
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <>
       <NavBar/>
+      <Footer/>
     </>
   );
 }
