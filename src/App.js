fd656a07b4dfd8a2db7e03aa8d73524847561e4b
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-10 15:16:17 -0300

    add contact form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import NavBar from './components/NavBar';
 import "./assets/styles/global.css"
 import Footer from './components/Footer';
 import Welcome from './components/views/Welcome';
+import Contact from './components/views/Contact';
 
 const App = () => {
   return (
     <>
       <NavBar/>
       <Welcome/>
+      <Contact/>
       <Footer/>
     </>
   );
