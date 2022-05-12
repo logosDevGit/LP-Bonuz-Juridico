e22a10c941bae88ad2ea79488b9edffb690948b5
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-12 14:17:12 -0300

    finish packages

--- src/components/views/Package/index.js
+++ src/components/views/Package/index.js
@@ -17,11 +17,12 @@ const Package = () => {
             <div className="sec-2">
                 <h2>Nossos Pacotes</h2>
                 <p>Adquira uns dos pacotes Bonuz, ganhe créditos BNZ's (nossa moeda virtual) e troque por serviços. </p>
-                <div className="cards">
+                <div className="cards mb-3">
                     {packageProps.map((card) => (
                         <PackageCard card={card}/> 
                     ))}
                 </div>
+                <small className="my-3">*Os Pacotes BNZ variam de acordo com a necessidade do seu serviço.</small>
             </div>
         </div>
     )
