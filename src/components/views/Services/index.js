607c7a6788029d7dab99f2c3b75ed7eadc1d164c
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-13 11:14:48 -0300

    starting flip function

--- src/components/views/Services/index.js
+++ src/components/views/Services/index.js
@@ -3,18 +3,33 @@ import { services } from '../../../helpers/services.js';
 
 const Services = () => {
 
+
+    const flipCard = (id) => {
+        const card = document.getElementById('service_'+id);
+
+        console.log(card)
+    }
+
     return(
         <div className="services py-5">
             <div className='wd-cont'>
                 <h2>Serviços</h2>
                 <div className='cards'>
-                    {services.map(({title, description, icon}) => (
-                        <div key={title} className="card">
-                            <div className='icon'>
-                                <img src={icon} alt={description}/>
+                    {services.map(({title, description, icon}, count = 1) => (
+                        <div key={title} id={'service_'+count} onClick={() => flipCard(count)}>
+                            <div className="card front">
+                                <div className='icon'>
+                                    <img src={icon} alt={description}/>
+                                </div>
+                                <h6>{title}</h6>
+                                <span>Ver mais</span>
                             </div>
-                            <h6>{title}</h6>
-                            <span>Ver mais</span>
+                            <div className="card back">
+                                <div className='icon'>
+                                    <img src={icon} alt={description}/>
+                                </div>
+                                <p className='my-auto px-1' style={{fontSize: "14px"}}>{description}</p>
+                            </div>
                         </div>
                     ))}
                 </div>
