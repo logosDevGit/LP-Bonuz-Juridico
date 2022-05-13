8317a46b67e95902fcd6ed0fd3188c22731e9399
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-13 12:03:01 -0300

    add flipped function to serviceCard

--- src/components/views/Services/index.js
+++ src/components/views/Services/index.js
@@ -1,36 +1,15 @@
 import './styles.css';
 import { services } from '../../../helpers/services.js';
+import ServiceCard from '../../ServiceCard';
 
 const Services = () => {
-
-
-    const flipCard = (id) => {
-        const card = document.getElementById('service_'+id);
-
-        console.log(card)
-    }
-
     return(
         <div className="services py-5">
             <div className='wd-cont'>
                 <h2>Serviços</h2>
                 <div className='cards'>
-                    {services.map(({title, description, icon}, count = 1) => (
-                        <div key={title} id={'service_'+count} onClick={() => flipCard(count)}>
-                            <div className="card front">
-                                <div className='icon'>
-                                    <img src={icon} alt={description}/>
-                                </div>
-                                <h6>{title}</h6>
-                                <span>Ver mais</span>
-                            </div>
-                            <div className="card back">
-                                <div className='icon'>
-                                    <img src={icon} alt={description}/>
-                                </div>
-                                <p className='my-auto px-1' style={{fontSize: "14px"}}>{description}</p>
-                            </div>
-                        </div>
+                    {services.map((service) => (
+                        <ServiceCard service={service} key={service.id}/>
                     ))}
                 </div>
             </div>
