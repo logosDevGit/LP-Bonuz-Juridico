f40d5b3299dc0e323da2f3f2c8b70ef825caadc8
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-25 17:29:41 -0300

    adjusting video

--- src/components/views/Media/index.js
+++ src/components/views/Media/index.js
@@ -7,7 +7,7 @@ const Media = () => {
     return (
         <div className="media wd-cont">
             <div className="video">
-                <iframe src="https://www.youtube.com/embed/cWDJoK8zw58"></iframe>
+                <iframe src="https://www.youtube.com/embed/99Hg0XC4nWE?controls=0&rel=0&playsinline=0&modestbranding=0&autoplay=0&enablejsapi=1&origin=https%3A%2F%2Fwww.bonuz.it&widgetid=1"></iframe>
             </div>
             <div className='icon dots-1'><img src={Dots} alt="pontinhos"/></div>
             <div className='icon dots-2'><img src={Dots} alt="pontinhos"/></div>
