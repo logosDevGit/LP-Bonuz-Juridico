810d8523d82369a078082187be06f4992e9b4a51
JoaoVitorLima242 <jlima0500@gmail.com>
2022-06-01 14:58:36 -0300

    adjusting btn href

--- src/components/views/Welcome/index.js
+++ src/components/views/Welcome/index.js
@@ -7,7 +7,7 @@ const Welcome = () => {
             <div className="cont-1">
                 <h1 className="my-4">Simplifique seu <span>jurídico</span>.</h1>
                 <p className="my-4">A Bonuz é uma plataforma tecnológica de intermediação de serviços jurídicos consultivos, que oferece qualidade a um custo acessível.</p>
-                <a className="btn btn-red my-4" rel="noreferrer" target="_blank" href="https://www.bonuz.it//">Consultar planos</a>
+                <a className="btn btn-red my-4" rel="noreferrer" target="_blank" href="https://hubscontabilidade.com.br/clubehubs/">Consultar planos</a>
             </div>
             <div>
                 <img src={Owl} alt='Coruja aplicativo Hub'/>
